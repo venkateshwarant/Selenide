b2a5137342b7be047ea07f8d1d9a9bc133ee072d
Venkateshwaran <venkateshwaran@Venkateshwarans-MacBook-Pro.local>
2020-05-01 12:39:34 +0200

    run test using maven

--- product.helloworld.testing.selenide/src/test/java/TestFirstServlet.java
+++ product.helloworld.testing.selenide/src/test/java/TestFirstServlet.java
@@ -12,13 +12,13 @@ public class TestFirstServlet {
 	@Test
 	public void testContent() {
 	  open(serverBaseURL+webpageURI);
-	  $("#hi").shouldHave(text("Hi There!")); // Waits until element gets text
+	  $("#hi").shouldHave(text("Hi There!"));
 	}
 	
-	@Test //This should fail as we are asserting wrong time with that of the one shown in the page
+	@Test
 	public void testTime(){
 		open(serverBaseURL+webpageURI);
-		$("#date").shouldHave(text("Date=Fri Apr 10 10:30:46 UTC 2020")); // Waits until element gets text
+		$("#date").shouldHave(text("Date=Fri Apr 10 10:30:46 UTC 2020"));
 	}
 	
 }
